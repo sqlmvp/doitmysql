193a770623f3c794df641626c3dd00927965b984
Sungwook Kang <kangstar05@hotmail.com>
2023-11-02 23:45:31 +0900

    .

--- 02_initial_data.sql
+++ 02_initial_data.sql
@@ -1,8 +1,23 @@
-insert into nasdaq_company (symbol, company_name, ipo_year, sector, industry) values('TSLA', 'Tesla Inc. Common Stock', 2010, 'Capital Goods', 'K2');
-insert into nasdaq_company (symbol, company_name, ipo_year, sector, industry) values('MSFT', 'Microsoft Corporation Common Stock', 1986, 'Technology', 'K2');
-insert into nasdaq_company (symbol, company_name, ipo_year, sector, industry)values('AMZN', 'Amazon.com Inc. Common Stock', 1997, 'Consumer Services', 'K2');
-insert into nasdaq_company (symbol, company_name, ipo_year, sector, industry) values('AAPL', 'Apple Inc. Common Stock', 1980, 'Technology', 'K2');
-insert into nasdaq_company (symbol, company_name, ipo_year, sector, industry) values('INTC', 'Intel Corporation Common Stock', NULL, 'Technology', 'K2');
-insert into nasdaq_company (symbol, company_name, ipo_year, sector, industry) values('NVDA', 'NVIDIA Corporation Common Stock', 1999, 'Technology', 'K2');
-insert into nasdaq_company (symbol, company_name, ipo_year, sector, industry) values('AMD', 'Advanced Micro Devices Inc. Common Stock', NULL, 'Technology', 'K2');
-insert into nasdaq_company (symbol, company_name, ipo_year, sector, industry) values('FB', 'Facebook Inc. Class A Common Stock', 2012, 'Technology', 'K2');
+insert into nasdaq_company (symbol, company_name, ipo_year, sector, industry) values ('TSLA', 'Tesla Inc. Common Stock', 2010, 'Capital Goods', 'K2');
+
+insert into nasdaq_company (symbol, company_name, ipo_year, sector, industry) values ('MSFT', 'Microsoft Corporation Common Stock', 1986, 'Technology', 'K2');
+
+insert into nasdaq_company (symbol, company_name, ipo_year, sector, industry) values ('AMZN', 'Amazon.com Inc. Common Stock', 1997, 'Consumer Services', 'K2');
+
+insert into nasdaq_company (symbol, company_name, ipo_year, sector, industry) values ('AAPL', 'Apple Inc. Common Stock', 1980, 'Technology', 'K2');
+
+insert into nasdaq_company (symbol, company_name, ipo_year, sector, industry) values ('INTC', 'Intel Corporation Common Stock', NULL, 'Technology', 'K2');
+
+insert into nasdaq_company (symbol, company_name, ipo_year, sector, industry) values ('NVDA', 'NVIDIA Corporation Common Stock', 1999, 'Technology', 'K2');
+
+insert into nasdaq_company (symbol, company_name, ipo_year, sector, industry) values ('AMD', 'Advanced Micro Devices Inc. Common Stock', NULL, 'Technology', 'K2');
+
+insert into nasdaq_company (symbol, company_name, ipo_year, sector, industry) values ('META', 'Meta Platforms, Inc.', 2012, 'Technology', 'K2');
+
+insert into nasdaq_company (symbol, company_name, ipo_year, sector, industry) values ('AMPG', 'AMPG, Inc.', 2012, '', 'K2');
+
+insert into nasdaq_company (symbol, company_name, ipo_year, sector, industry) values ('CAR', 'CAR, Inc.', 2012, '', 'K2');
+
+insert into nasdaq_company (symbol, company_name, ipo_year, sector, industry) values ('UAN', 'UAN, Inc.', 2012, '', 'K2');
+
+insert into nasdaq_company (symbol, company_name, ipo_year, sector, industry) values ('BHR', 'BHR, Inc.', 2012, '', 'K2');
